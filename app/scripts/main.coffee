9c755dc46782c516810e73b0dd79f736eabba429
vazquez <2ricardo.vazquez@gmail.com>
2014-02-02 17:26:07 -0500

    beginning front end design

---
+++ app/scripts/main.coffee
@@ -0,0 +1,2 @@
+jQuery ->
+  $('.hero-unit').fadeIn();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    'use strict';
+    $('.hero-unit h1').eq(1).hide().delay(500).fadeIn(1000);
+});
