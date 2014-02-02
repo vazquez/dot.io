e797687dd4e50edc02cc37a4f695f68521dc9f83
vazquez <2ricardo.vazquez@gmail.com>
2014-02-02 14:10:25 -0500

    removing grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,16 @@
 module.exports = function(grunt) {
 
     // 1. All configuration goes here
-    var config = {devserver:{server: {} }};
-    grunt.initConfig(config);
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        devserver: {
-            options: {
-                'port': 8888,
-            }
-        }
+
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    require('load-grunt-tasks')(grunt);
+    // require('load-grunt-tasks')(grunt);
+    // grunt.loadNpmTasks('grunt-devserver');
+
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['devserver']);
+    grunt.registerTask('default', ['']);
 
 };
