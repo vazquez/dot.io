acc917bb5742d6a9ab80b50b0dfe2969e8e3aae2
vazquez <2ricardo.vazquez@gmail.com>
2014-02-02 21:08:04 -0500

    Eploring alt layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,9 @@
 $(function() {
     'use strict';
+    var windowHeight = $(window).height();
+    // $('.hero-unit').css('min-height', windowHeight);
     $('.hero-unit h1').eq(0).hide().delay(500).fadeIn(1000);
     $('.hero-unit h1').eq(1).hide().delay(1500).fadeIn(1000);
     $('.hero-unit h1').eq(2).hide().delay(2500).fadeIn(1000);
     $('.hero-unit h1').eq(3).hide().delay(3500).fadeIn(1000);
-
-
-
 });
