2dfa542a5509c937a38983038a16cc9941e6bdca
vazquez <2ricardo.vazquez@gmail.com>
2014-02-05 23:01:47 -0500

    adding bower package: fitText.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,8 @@
 $(function() {
     'use strict';
-    $('.hero-unit h1').eq(0).hide().delay(500).fadeIn(1000);
-    $('.hero-unit h1').eq(1).hide().delay(1500).fadeIn(1000);
-    $('.hero-unit h1').eq(2).hide().delay(2500).fadeIn(1000);
-    $('.hero-unit h1').eq(3).hide().delay(3500).fadeIn(1000);
-
-
-
+    $('.hero-unit article').eq(0).hide().delay(500).fadeIn(1000);
+    $('.hero-unit article').eq(1).hide().delay(1500).fadeIn(1000);
+    $('.hero-unit article').eq(2).hide().delay(2500).fadeIn(1000);
+    $('.hero-unit article').eq(3).hide().delay(3500).fadeIn(1000);
+    $('.hero-unit article').fitText(1.7, { minFontSize: '30px'});
 });
