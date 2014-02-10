fdb7b3c76a4b92b71164dff46e3846dca8533e1b
vazquez <2ricardo.vazquez@gmail.com>
2014-02-09 21:55:43 -0500

    adding about text. typographic treatment.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,7 @@ $(function() {
     $('.hero-unit article').eq(2).hide().delay(2500).fadeIn(1000);
     $('.hero-unit article').eq(3).hide().delay(3500).fadeIn(1000);
     $('.hero-unit article').fitText(1.7, { minFontSize: '30px'});
+    $('.heading').fitText();
     $('nav').delay(2000).addClass('visible');
 
 });
